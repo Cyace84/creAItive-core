d80474471abaa4f206a00b7ff7134370cee61d8b
cyace84 <cyace84@gmail.com>
2023-03-30 18:46:39 +0200

    fix auth bug

--- src/api/middleware/auth.ts
+++ src/api/middleware/auth.ts
@@ -10,18 +10,23 @@ export default {
     res: Response,
     next: NextFunction,
   ) => {
-    const authHeader = req.headers["authorization"];
-    const token =
-      authHeader && authHeader.split(" ")[1].replace('"', "").replace('"', "");
+    try {
+      const authHeader = req.headers["authorization"];
+      const token =
+        authHeader &&
+        authHeader.split(" ")[1].replace('"', "").replace('"', "");
 
-    if (token == null) return res.sendStatus(401);
-    jwt.verify(token, config.JWT_SECRET, async (err: any, user: User) => {
-      const cachedUser = await cache.getUserFromEmail(user.email);
-      if (err) return res.sendStatus(403);
+      if (token == null) return res.sendStatus(401);
+      jwt.verify(token, config.JWT_SECRET, async (err: any, user: User) => {
+        const cachedUser = await cache.getUserFromEmail(user.email);
+        if (err) return res.sendStatus(403);
 
-      req.user = cachedUser;
-      next();
-    });
+        req.user = cachedUser;
+        next();
+      });
+    } catch (error) {
+      next(error);
+    }
   },
   getCallbackUrl: (req: Request, res: Response, next: NextFunction) => {
     const callbackUrl = req.query.callback as string;
